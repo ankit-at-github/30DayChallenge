32e463aba26eb744c91560476ac7d42abe470d17
ankitkumar <ankitkumar@localhost>
2022-01-14 21:35:54 -0500

    Problem 2 : Search in a 2-D Matrix

--- 30DayChallenge/src/Day4/Search2DMatrix.java
+++ 30DayChallenge/src/Day4/Search2DMatrix.java
@@ -5,41 +5,35 @@ public class Search2DMatrix {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-		
-		//Element to be searched in the 2-D matrix
 		int target = 5;
 		
-		//number of rows
-		int row = matrix.length;
-		//number of columns
-		int col = matrix[0].length;
+		//Can be solved using binary search as it is given that elements of each row is sorted and elements
+		//of each column are also sorted
 		
-		//Think it as numbers are stores in 1-D array and total elements will be row*col (indexed from 0)
+		//Approach 2: Start from top-right corner and check if element is greater or smaller
+		//if element is greater than target then increase row else decrease column
 		
-		int start = 0;
-		int end = (row*col) - 1;
+		int row = 0;
+        int col = matrix[0].length-1;
+        
+        boolean found = false;
+        
+        while(row < matrix.length && col >= 0)
+        {
+            if(matrix[row][col] == target)
+            {
+            	found = true;
+            	break;
+            }
+            else
+            {
+                if(matrix[row][col] > target) col--;
+                else row++;
+            }
+        }
+        
+        System.out.println(found);
 		
-		boolean found = false;
-		
-		while(start <= end)
-		{
-			int mid = start + (end-start)/2;
-			
-			//to get the mid index as index in 2-D array
-			
-			if(matrix[mid/col][mid%col] == target)
-			{
-				found = true;
-				break;
-			}
-			else
-			{
-				if(matrix[mid/col][mid%col] < target) start = mid+1;
-				else end = mid-1;
-			}
-		}
-		
-		System.out.println(found);
 	}
 
 }
